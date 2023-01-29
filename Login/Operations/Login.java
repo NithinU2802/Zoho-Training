cc9aa485bd6890041b91cee62ceac302ba4af003
Nithin U <106614289+NithinU2802@users.noreply.github.com>
2023-01-29 10:23:57 +0530

    Create Login.java

---
+++ Login/Operations/Login.java
@@ -0,0 +1,11 @@
+class Login{
+
+public boolean check(String d,String n){
+Register reg=new Register();
+for(int i=0;i<reg.id.size();i++){
+System.out.println(d+" "+reg.id.get(i));
+if(d.equals(reg.id.get(i))) return true;
+}
+return false;
+}
+}
